ce3f837118871c589de566be40f64f57b234a067
MarceloCMiguel <marcelocmiguel@outlook.com>
2021-11-23 20:13:04 -0300

    logs

--- functions.py
+++ functions.py
@@ -1,13 +1,15 @@
+from logging import log
 import os
-
+from log import logging
 #create a key_pair
+
 def create_key_pair(client,key_name:str):
     key_pair = client.create_key_pair(KeyName=key_name)
 
     private_key = key_pair["KeyMaterial"]
 
     # write private key to file with 400 permissions
-    print(f"Writing {key_name}.pem in directory ./")
+    logging.info(f"Writing {key_name}.pem in directory ./")
     with os.fdopen(os.open(f"./{key_name}.pem", os.O_WRONLY | os.O_CREAT, 0o400), "w+") as handle:
         handle.write(private_key)
     return key_pair
@@ -15,7 +17,7 @@ def create_key_pair(client,key_name:str):
 #delete a key_pair
 def delete_key_pair(client,key_name:str):
     key_pair = client.delete_key_pair(KeyName=key_name)
-    print(f"removing {key_name} if exist")
+    logging.warning(f"removing {key_name} if exist")
     # write private key to file with 400 permissions
     try:
         os.remove("./" + key_name + ".pem")
@@ -27,7 +29,7 @@ def delete_key_pair(client,key_name:str):
 def create_an_instance(client, ami,key_name,user_data,sec_group_name,sec_group_id):
     
     instancetype = "t2.micro"
-    print(f"Creating an instance with {instancetype}, and keyname {key_name['KeyName']}")
+    logging.info(f"Creating an instance with {instancetype}, and keyname {key_name['KeyName']}")
     if user_data == None:
         instances = client.run_instances(
             ImageId=ami,
@@ -69,7 +71,7 @@ def create_an_instance(client, ami,key_name,user_data,sec_group_name,sec_group_i
         },
     ],
     )['Reservations'][0]['Instances'][0]['PublicIpAddress']
-    print(f"Instance id: {instance_id}\nInstance Public IP:{instance_ip}")
+    logging.info(f"Instance id: {instance_id}. Instance Public IP:{instance_ip}")
     return instance_id, instance_ip
 
 def delete_instances(client,key_pair):
@@ -91,15 +93,16 @@ def delete_instances(client,key_pair):
 )['Reservations']:
         list_instances_id.append(i['Instances'][0]["InstanceId"])
     if len(list_instances_id) > 0:
-        print(f"Deleting Instances ids {list_instances_id}")
+        logging.warning(f"Deleting Instances ids {list_instances_id}")
         waiter = client.get_waiter('instance_terminated')
         client.terminate_instances(InstanceIds=list_instances_id)
         waiter.wait(InstanceIds=list_instances_id)
-        print(f"Instances withs keypair {key_pair} deleted")
+        logging.warning(f"Instances with keypair {key_pair} deleted")
     else:
-        print(f"There is no instances running with the keypair {key_pair}")
+        logging.info(f"There is no instances running with the keypair {key_pair}")
         
 def delete_sec_group(client,sec_group_name):
+    logging.warning(f"Deleting {sec_group_name} if exist")
     find_sec_group = False
     security_groups = client.describe_security_groups()
     for sec_group in security_groups['SecurityGroups']:
@@ -109,15 +112,14 @@ def delete_sec_group(client,sec_group_name):
             while is_deleted ==False:
                 try:
                     deleted_sec_group = client.delete_security_group(GroupName=sec_group["GroupName"], GroupId=sec_group["GroupId"])
-                    #print(deleted_sec_group)
                     is_deleted = True
                 except Exception as e:
                     pass
 
     if find_sec_group:
-        print(f"{sec_group_name} deleted")
+        logging.warning(f"{sec_group_name} deleted")
     else:
-        print(f"{sec_group_name} not found")
+        logging.info(f"{sec_group_name} not found")
 
 def create_security_group(client, SEC_GROUP_NAME,IP_PERMISSIONS):
     vpcs = client.describe_vpcs()
@@ -127,25 +129,27 @@ def create_security_group(client, SEC_GROUP_NAME,IP_PERMISSIONS):
                                                     Description="Created with boto3",
                                                     VpcId=vpc_id)
     except Exception as e:
-        print(e)
+        logging.info(e)
 
     security_group_id = security_group['GroupId']
     client.authorize_security_group_ingress(
         GroupId=security_group_id,
         IpPermissions = IP_PERMISSIONS
     )
-    print(f"Security Group {SEC_GROUP_NAME} created")
+    logging.info(f"Security Group {SEC_GROUP_NAME} created")
     return security_group_id
 
 def create_image(client,instance_id,name_image):
+    logging.info(f"Creating Image (AMI) {name_image} using instance ID {instance_id}")
     image_response=client.create_image(InstanceId=instance_id,Name=name_image)
     waiter = client.get_waiter('image_available')
     image_id = image_response["ImageId"]
     waiter.wait(ImageIds=[image_id])
-    print(f"Image {name_image} created with id {image_id}")
+    logging.info(f"Image {name_image} created with id {image_id}")
     return image_id
     
 def delete_image(client,image_name):
+    logging.warning(f"Deleting {image_name} if exist")
     images_described=client.describe_images(Filters=[
         {
             'Name': 'name',
@@ -155,11 +159,11 @@ def delete_image(client,image_name):
         },
     ])
     if len(images_described['Images'])<1:
-        print(f"There is no image with name {image_name}")
+        logging.info(f"There is no image with name {image_name}")
         return
     image_id = images_described['Images'][0]['ImageId']
     client.deregister_image(ImageId=image_id)
-    print(f"Image {image_name} deleted")
+    logging.warning(f"Image {image_name} deleted")
     
     
 def create_load_balancer(client,client_lb, lb_name,sec_group_id):    
@@ -168,28 +172,29 @@ def create_load_balancer(client,client_lb, lb_name,sec_group_id):
     for subnet in subnets['Subnets']:
         list_subnets_id.append(subnet['SubnetId'])
     waiter = client_lb.get_waiter('load_balancer_available')    
-    print("Creating load balancer with all subnets available")
+    logging.info(f"Creating load balancer called {lb_name} with all subnets available")
     load_balancer_created = client_lb.create_load_balancer(Name = lb_name,
                                                                     Subnets=list_subnets_id,
                                                                     SecurityGroups=[sec_group_id],
                                                                     IpAddressType="ipv4",)
     LoadBalancerArn_ = load_balancer_created['LoadBalancers'][0]['LoadBalancerArn']
     waiter.wait(LoadBalancerArns=[LoadBalancerArn_])
-    print(f"Load Balancer {lb_name} created")
+    logging.info(f"Load Balancer {lb_name} created")
     return LoadBalancerArn_
 
 def delete_load_balancer(client_lb,lb_name):
+    logging.warning(f"Deleting Load Balancer {lb_name} if exist")
     waiter = client_lb.get_waiter('load_balancers_deleted')
     try:
         load_balancers = client_lb.describe_load_balancers(Names=[lb_name])
     except:
-        print(f"No load_balancers with name {lb_name}")
+        logging.info(f"No Load Balancers with name {lb_name}")
         return
     for lb in load_balancers['LoadBalancers']:
         client_lb.delete_load_balancer(LoadBalancerArn=lb['LoadBalancerArn'])
         lb_arn = lb['LoadBalancerArn']
         waiter.wait(LoadBalancerArns=[lb['LoadBalancerArn']])
-        print(f"Load Balancer {lb['LoadBalancerArn']} with name {lb_name} deleted")
+        logging.warning(f"Load Balancer {lb_name} with Arn {lb['LoadBalancerArn']} deleted")
         
     
     return lb_arn
@@ -205,20 +210,22 @@ def create_target_group(client_ec2,client_lb,tg_name):
         HealthCheckPath='/tasks/'
     )
     tg_arn =tg_created['TargetGroups'][0]['TargetGroupArn']
-    print(f"Target group {tg_name} with arn {tg_arn} created")
+    logging.info(f"Target group {tg_name} with Arn {tg_arn} created")
     return tg_arn
 
 def delete_target_group(client_lb,tg_name):
+    logging.warning(f"Deleting Target Group {tg_name} if exist")
     try:
         tgs=client_lb.describe_target_groups(Names=[tg_name])
     except:
-        print(f"No target group with name {tg_name}")
+        logging.info(f"No target group with name {tg_name}")
         return
     tg_arn = tgs['TargetGroups'][0]['TargetGroupArn']
     client_lb.delete_target_group(TargetGroupArn = tg_arn)
-    print(f"Target Group {tg_name} deleted")
+    logging.warning(f"Target Group {tg_name} deleted")
     
-def create_launch_configuration(as_client,launch_config_name,image_id, sec_group_id,key_name):    
+def create_launch_configuration(as_client,launch_config_name,image_id, sec_group_id,key_name):
+    logging.info(f"Creating a Launch Configuration {launch_config_name}")   
     try:
         
         as_client.create_launch_configuration(
@@ -229,22 +236,23 @@ def create_launch_configuration(as_client,launch_config_name,image_id, sec_group
             KeyName=key_name
         )
     except:
-        print(f"A launch configuration already exists with the name {launch_config_name}")
+        logging.info(f"A launch configuration already exists with the name {launch_config_name}")
         return
-    print(f"Launch Configuration {launch_config_name} created")
+    logging.info(f"Launch Configuration {launch_config_name} created")
     
 def delete_launch_configuration(as_client,launch_config_name):
+    logging.warning(f"Deleting Launch Configuration {launch_config_name} if exist")
     try:        
         as_client.delete_launch_configuration(
             LaunchConfigurationName = launch_config_name
         )
     except:
-        print(f"Launch configuration {launch_config_name} not found")
+        logging.info(f"Launch Configuration {launch_config_name} not found")
         return
-    print(f"Launch configuration {launch_config_name} deleted")
+    logging.warning(f"Launch Configuration {launch_config_name} deleted")
 
 def create_auto_scaling_group(client_ec2,client_as,as_name,launch_config_name,tg_arn):
-    
+    logging.info(f"Creating AutoScaling group with name {as_name}")
     list_zones = []
     for zones in client_ec2.describe_availability_zones()['AvailabilityZones']:
         list_zones.append(zones['ZoneName'])
@@ -256,15 +264,18 @@ def create_auto_scaling_group(client_ec2,client_as,as_name,launch_config_name,tg
         TargetGroupARNs=[tg_arn],
         AvailabilityZones = list_zones
     )
-    print(f"Auto scaling group {as_name} created")
+    logging.info(f"Auto scaling group {as_name} created")
 
 def delete_auto_scaling_group(as_client,as_name):
+    logging.warning(f"Deleting AutoScaling group {as_name} if exist")
     try:
         as_client.delete_auto_scaling_group(AutoScalingGroupName = as_name,ForceDelete= True)
+        logging.warning(f"AutoScaling group {as_name} deleted")
     except:
-        print(f"{as_name} not found")
+        logging.info(f"{as_name} not found")
         
 def create_listener(lb_client,load_balancer_arn,tg_arn):
+    logging.info("Creating listener to port 80")
     listener_created=lb_client.create_listener(
         LoadBalancerArn= load_balancer_arn,
         Protocol='HTTP',
@@ -281,12 +292,12 @@ def create_listener(lb_client,load_balancer_arn,tg_arn):
     return listener_arn
         
 def delete_listeners(client_lb, lb_arn):
-    
+    logging.warning(f"Deleting all listener with LoadBalancer Arn {lb_arn}")
     try:
         listeners=client_lb.describe_listeners(LoadBalancerArn= lb_arn)
     except:
-        print(f"There is no Listener created with LoadBalancerArn {lb_arn}")
+        logging.info(f"There is no Listener created with LoadBalancerArn {lb_arn}")
         return
     for listener in listeners['Listeners']:
         client_lb.delete_listener(ListenerArn = listener['ListenerArn'])
-    print(f"Listeners with arn {lb_arn} deleted")
+    logging.warning(f"Listeners with arn {lb_arn} deleted")
---
+++ log.py
@@ -0,0 +1,3 @@
+import logging
+
+logging.basicConfig(filename='script.log', filemode='w',format='%(asctime)s - %(levelname)s - %(message)s',level=logging.INFO)
